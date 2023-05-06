776cedb4426e189d4bd0b644c638dac5e1161656
Marvin <124067182+babi-nos@users.noreply.github.com>
2023-05-06 15:49:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function guessMyNumber() {
   } else {
     result.innerHTML = `Perdu: le nombre était <span class="green" >${nombre} </span>. Vous aviez choisi <span>${valeur}</span>. Réessayer !!`;
   }
-  
+
   monInput.value = "";
 }
 
@@ -21,3 +21,33 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   guessMyNumber();
 });
+
+const nomberMaker = () => {
+  const nomber = document.createElement("span");
+  document.body.appendChild(nomber);
+  nomber.classList.add("nomber");
+
+  const nomberRandom = Math.floor(Math.random() * 10);
+
+  nomber.innerHTML = nomberRandom;
+
+  nomber.style.top = Math.random() * -100  + "%";
+  nomber.style.left = Math.random() * 100 + "%";
+  
+  
+  const size = Math.random() * 35 + 20 + "px";
+
+
+  nomber.style.fontSize = size;
+  // nomber.style.width = size;
+
+  nomber.style.setProperty("--left", Math.random() * 100 + "%");
+  nomber.style.setProperty("--rotate", Math.floor(Math.random() * 721) - 360 + "deg");
+
+
+  setTimeout(() => {
+    nomber.remove();
+  }, 12000);
+};
+
+setInterval(nomberMaker, 200);
